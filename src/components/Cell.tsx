5c52cb2eccc397d332aed2cd749557d7c3b118dc
Oskar Barcz <ob@gi.org.pl>
2024-12-07 20:58:41 +0100

    Add labirynth login generator

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -4,17 +4,16 @@ type CellProps = {
   value: CellValue,
 }
 
-export const Cell = ({ value }: CellProps) => {
-  switch (value) {
-    case CellValue.Path:
-      return <div className="w-4 h-4 bg-gray-500 inline-block"></div>;
-    case CellValue.Player:
-      return <div className="w-4 h-4 bg-red-500 inline-block"></div>;
-    case CellValue.Goal:
-      return <div className="w-4 h-4 bg-cyan-500 inline-block"></div>;
-    default:
-      return <div className="w-4 h-4 bg-gray-200 inline-block"></div>;
-  }
+const colors = {
+  [CellValue.Path]: 'bg-gray-500',
+  [CellValue.Player]: 'bg-red-500',
+  [CellValue.Goal]: 'bg-cyan-500',
+  [CellValue.Wall]: 'bg-gray-200',
+}
 
+export const Cell = ({ value }: CellProps) => {
+  const color = colors[value];
+
+  return <div className={`w-4 h-4 ${color} inline-block`}></div>;
 }
 
