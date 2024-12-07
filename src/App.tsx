10451dbdf7f9f09c5f47c8472a3eab0f5ad1fcfe
kodowiec <62697517+kodowiec@users.noreply.github.com>
2024-12-07 21:51:39 +0100

    handle map switching

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { drawFromBitmap } from './service/functions/drawBoard.tsx';
 import { Grid } from './components/Grid.tsx';
 import { useEffect, useRef, useState } from 'react';
 import { bitmapService } from './service/bitmap.service.tsx';
-import {Bitmap} from "./type/bitmap.type.ts";
-import {BoardService} from "./service/board.service.ts";
+import { Bitmap } from "./type/bitmap.type.ts";
+import { BoardService } from "./service/board.service.ts";
+import firstmapfile from './assets/logo.txt';
 
 export default function App() {
   const [bitmap, setBitmap] = useState<Bitmap>([]);
   const [board, setBoard] = useState<BoardService | null>(null);
   const boardRef = useRef<BoardService>(new BoardService([]));
 
+  let currentMap = firstmapfile;
+
   // Load bitmap on mount
   useEffect(() => {
-    bitmapService().then((loadedBitmap: Bitmap) => {
+    bitmapService(firstmapfile).then((loadedBitmap: Bitmap) => {
       setBitmap(loadedBitmap);
     });
   }, []);
@@ -33,7 +36,17 @@ export default function App() {
     setBoard(updatedBoard);
 
     const handleKeyUp = (event: KeyboardEvent) => {
-      boardRef.current.movePlayer(event);
+      try {
+        boardRef.current.movePlayer(event);
+      }
+      catch (e) {
+        if ((e as Error).message == "You win!") {
+          currentMap = 'src/assets/map.txt';
+          bitmapService(currentMap).then((loadedBitmap: Bitmap) => {
+            setBitmap(loadedBitmap);
+          });
+        }
+      }
       setBoard(new BoardService(boardRef.current.getBoard()));
     };
 
--- src/service/bitmap.service.tsx
+++ src/service/bitmap.service.tsx
@@ -1,9 +1,8 @@
-import textfile from "../assets/logo.txt";
 import {Bitmap} from "../type/bitmap.type.ts";
 
 
-export const bitmapService = async (): Promise<Bitmap> => {
-  const response = await fetch(textfile);
+export const bitmapService = async (mapfile : string): Promise<Bitmap> => {
+  const response = await fetch(mapfile);
   const text = await response.text();
   const lines = text.split('\n');
   const data = lines.map(line => line.split(''));
