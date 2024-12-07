f0ce4e53c8e6443f3d1e09bb8a9390ab5db5ce38
kodowiec <62697517+kodowiec@users.noreply.github.com>
2024-12-07 23:08:28 +0100

    fog

--- src/service/functions/drawBoard.tsx
+++ src/service/functions/drawBoard.tsx
@@ -3,7 +3,7 @@ import {Bitmap, BitmapCell, BitmapRow} from "../../type/bitmap.type.ts";
 import {BoardService} from "../board.service.ts";
 
 export const drawFromBitmap = (bitmap: Bitmap): BoardService => {
-  const map: Board = bitmap.map((row: BitmapRow) => row.map((column: BitmapCell) => column)) as unknown as Board;
+  const map: Board = bitmap.map((row: BitmapRow) => row.map((column: BitmapCell) =>  ({type: column, visited: false, fog: true}))) as unknown as Board;
 
   return new BoardService(map);
 };
