6fb45a86c451db729bd5927c87a5d707b21ed61c
Oskar Barcz <ob@gi.org.pl>
2024-12-08 10:33:13 +0100

    Fix audio resource path

---
+++ src/components/EggInfo/EggInfo.tsx
@@ -0,0 +1,8 @@
+export function EggInfo() {
+  return (
+    <div className="pt-6">
+      <p>these eggs are so tasty,</p>
+      <p>let's collect them!</p>
+    </div>
+  );
+}
--- src/components/WarningContainer/WarningContainer.tsx
+++ src/components/WarningContainer/WarningContainer.tsx
@@ -1,6 +1,7 @@
 import {BombWarning} from "../BombWarning/BombWarning.tsx";
 import {FogWarning} from "../FogWarning/FogWarning.tsx";
 import {FogPreWarning} from "../FogPreWarning/FogPreWarning.tsx";
+import {EggInfo} from "../EggInfo/EggInfo.tsx";
 
 type WarningContainerProps = {
   mapIndex: number
@@ -8,6 +9,8 @@ type WarningContainerProps = {
 
 export function WarningContainer({mapIndex}: WarningContainerProps) {
   switch (mapIndex) {
+    case 0:
+      return <EggInfo/>
     case 1:
       return <BombWarning/>
     case 2:
