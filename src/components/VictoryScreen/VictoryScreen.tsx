68a6bd58b6f3808a7241a009a7eaf8ef1177524c
kodowiec <62697517+kodowiec@users.noreply.github.com>
2024-12-09 03:04:55 +0100

    add victory screen component

---
+++ src/components/VictoryScreen/VictoryScreen.tsx
@@ -0,0 +1,8 @@
+export function VictoryScreen() {
+  return (
+    <div className="pt-6">
+      <p>it seems like you have found all lost eggs</p>
+      <p>but your princess is in another castle...</p>
+    </div>
+  );
+}
